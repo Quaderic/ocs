099f9826add70288999f931f44455444e8743f9d
Eric Kuada <eric@eric-cloudstack.(none)>
2013-07-14 10:28:45 +0200

    Inital or First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OCS::Application.config.secret_key_base = '4b210b21bba238a978f2cf992a3d293c8995335f016ef474edd5ce72abc0f6c7edd8d37cedf25dca4383f08d1c564e1676f1cb180eb917a7764386d85473c570'
